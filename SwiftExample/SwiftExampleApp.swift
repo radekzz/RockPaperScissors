f3a713e0df1946f7e317f67f427cd5022b8d632a
radekzz <radek@mezulanik.cz>
2022-03-02 09:13:04 +0100

    Initial Commit

---
+++ SwiftExample/SwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExampleApp.swift
+//  SwiftExample
+//
+//  Created by Radek Mezulanik on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
